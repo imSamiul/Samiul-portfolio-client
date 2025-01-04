d27d4f6f63593305d2e2b98ed0059702f7952b84
Samiul Karim <samiulkarimprodhan@gmail.com>
2025-01-04 23:50:03 +0600

    feat: remove TypeScript.svg asset and update CardLoader layout for improved responsiveness

--- src/components/UI/CardLoader.tsx
+++ src/components/UI/CardLoader.tsx
@@ -1,25 +1,25 @@
 function CardLoader() {
   return (
-    <div className="flex justify-between flex-wrap gap-5">
-      <div className="flex w-52 flex-col gap-4">
+    <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-between flex-wrap gap-5">
+      <div className="flex w-full flex-col gap-4">
         <div className="skeleton h-32 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
-      <div className="flex w-52 flex-col gap-4">
+      <div className="flex w-full flex-col gap-4">
         <div className="skeleton h-32 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
-      <div className="flex w-52 flex-col gap-4">
+      <div className="flex w-full flex-col gap-4">
         <div className="skeleton h-32 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
-      <div className="flex w-52 flex-col gap-4">
+      <div className="flex w-full flex-col gap-4">
         <div className="skeleton h-32 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
