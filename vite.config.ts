1a2c815b4dc5c94a7df0c4d0ec0f4c0d172654fc
Samiul Karim <samiulkarimprodhan@gmail.com>
2025-01-05 17:18:50 +0600

    fix: add proxy configuration for API requests in Vite server

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://samiuls-portfolio-server.vercel.app", // Your backend server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
